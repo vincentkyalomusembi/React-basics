2971c719b58971b3788dba63ab7c5cdede33cf5c
vincentkyalomusembi <vincentkyalo822@gmail.com>
2025-04-04 13:49:51 +0300

    js logic

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -71,6 +71,15 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("we're curently opened");
+  // else alert("Sorry we're closed");
+
   return (
     <footer>{new Date().toLocaleTimeString()}. we're currently open</footer>
   );
