c43f08ac0575df6f934047b659e7de9439f117ae
vincentkyalomusembi <vincentkyalo822@gmail.com>
2025-04-06 21:27:04 +0300

    props

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -72,10 +72,24 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredient="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price="$12"
+      />
     </main>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h3>Pizza Spinaci</h3>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+      <h3>$12</h3>
+    </div>
   );
 }
 
@@ -96,16 +110,6 @@ function Footer() {
   );
 }
 
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-      <h3>$12</h3>
-    </div>
-  );
-}
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
