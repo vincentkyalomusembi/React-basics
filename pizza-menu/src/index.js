5aa66c2313aa3e18000b77e6f35f580948e27b20
vincentkyalomusembi <vincentkyalomusembi@users.noreply.github.com>
2025-05-16 22:21:41 +0300

    destructuring props

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -102,18 +102,18 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  console.log(props);
+function Pizza({pizzaObj}) {
+  console.log(pizzaObj);
 
-  if (props.pizzaObj.soldOut) return null;
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <h3>{props.pizzaObj.price}</h3>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <h3>{pizzaObj.price}</h3>
       </div>
     </li>
   );
@@ -136,15 +136,23 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online</p>
-          <button className="btn">order</button>
-        </div>
-      ) : <p>we're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>}
+        <Order closeHour={closeHour} openHour={openHour}/>
+      ) : (
+      <p>
+        we're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>)}
     </footer>
   );
 }
 
+function Order({closeHour, openHour}) {
+  return (
+  <div className="order">
+     <p>We're open until {closeHour}:00. Come visit us or order online</p>
+     <button className="btn">order</button>
+  </div>
+  );
+}
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
