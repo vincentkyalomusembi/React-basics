ae670e9a7c34f9d8790138966ca016e90ab787ff
vin <vin@localhost.localdomain>
2025-05-16 10:45:54 +0300

    conditional rendering

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -106,7 +106,7 @@ function Pizza(props) {
   console.log(props);
 
   if (props.pizzaObj.soldOut) return null;
-  
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
